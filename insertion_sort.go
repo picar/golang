eb75ae823e44d12dcd7b6e1179f84edb6691a853
Pierre Carette <pierre.carette@gmail.com>
2016-12-28 18:56:30 +0000

    Add TimeNano display

--- insertion_sort.go
+++ insertion_sort.go
@@ -13,14 +13,12 @@ func main() {
 	var slice []int
 	slice = make([]int, r.Intn(32))
 
-	fmt.Println("len(slice):", len(slice), ",slice:", slice)
-
 	for i := 0; i < len(slice); i++ {
 		slice[i] = r.Intn(256)
 	}
 
 	fmt.Println("len(slice):", len(slice), ",slice:", slice)
-
+	start := time.Now().UnixNano()
 	for i := 0; i < len(slice); i++ {
 		for j := i; j > 0; j-- {
 			if slice[j] < slice[j-1] {
@@ -30,6 +28,7 @@ func main() {
 			}
 		}
 	}
-
+	time := time.Now().UnixNano() - start
 	fmt.Println("len(slice):", len(slice), ",slice:", slice)
+	fmt.Println("TimeNano:", time)
 }
